ee083779dc5584d64a77574fd6f51c955de76712
Jose Mesa <jose.mesa.solorzano@gmail.com>
2018-07-13 13:51:43 +0200

    s

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const baseUrl = 'http://localhost:4000'
+// export const baseUrl = 'http://localhost:4000'
 
-// export const baseUrl = 'https://whispering-tundra-57382.herokuapp.com'
+export const baseUrl = 'https://whispering-tundra-57382.herokuapp.com'
 
 
 export const localStorageJwtKey = 'currentUserJwt'
